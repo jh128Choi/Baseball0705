5f044adc447ddad4682156a5bbffa80dfe88b1b7
JunhyeokChoi <anchovy1228@naver.com>
2023-07-05 17:01:37 +0900

    feature 2

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -11,5 +11,13 @@ public:
 		{
 			throw length_error("Must be three letters.");
 		}
+
+		for (char ch : string)
+		{
+			if (ch < '0' || ch > '9')
+			{
+				throw invalid_argument("Must be number");
+			}
+		}
 	}
 };
