f8e665bfd869c7fa413e13e1e88b6bc229ac3233
JunhyeokChoi <anchovy1228@naver.com>
2023-07-05 17:11:23 +0900

    refactor 3

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -5,7 +5,14 @@ using namespace std;
 class Baseball
 {
 public:
-	void guess(const string& guessNumber)
+	bool isDuplicatedNumber(const string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1] ||
+			guessNumber[1] == guessNumber[2] ||
+			guessNumber[2] == guessNumber[0];
+	}
+
+	void assertIllegalArgument(const string& guessNumber)
 	{
 		if (guessNumber.length() != 3)
 		{
@@ -14,17 +21,19 @@ public:
 
 		for (char ch : guessNumber)
 		{
-			if (ch < '0' || ch > '9')
-			{
-				throw invalid_argument("Must be number");
-			}
+			if (ch >= '0' && ch <= '9')
+				continue;
+			throw invalid_argument("Must be number");
 		}
 
-		if (guessNumber[0] == guessNumber[1] ||
-			guessNumber[1] == guessNumber[2] ||
-			guessNumber[2] == guessNumber[0])
+		if (isDuplicatedNumber(guessNumber))
 		{
 			throw invalid_argument("Must not have the same number");
 		}
+	}
+
+	void guess(const string& guessNumber)
+	{
+		assertIllegalArgument(guessNumber);
 	}
 };
